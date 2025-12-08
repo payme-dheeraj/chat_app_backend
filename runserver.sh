a89fe7356a8c819b4f125ebebdbaf519701146c2
payme-dheeraj <dheeraj@paymeindia.in>
2025-12-08 15:21:26 +0530

    [add] render deployment config

--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 pip install -r requirements.txt
+python manage.py runserver 0.0.0.0:8000
 python manage.py migrate
 python manage.py collectstatic --noinput
+python manage.py createsuperuser --username $ADMIN_USERNAME --email $ADMIN_EMAIL --password $ADMIN_PASSWORD
